092bd575c47fbaee9a993df912d2bcf450d3641f
AchlaSB <achalasbhatt@gmail.com>
2019-01-22 19:23:58 +0530

    initial commit

---
+++ app/config.js
@@ -0,0 +1,24 @@
+const onServer = !!process.env["DYNO"];
+const homeDir = process.env["HOME"];
+const wasmceptionDir = process.env["WASMCEPTION"] ||
+    (__dirname + '/../wasmception');
+
+exports.sysroot = onServer ?
+  homeDir + '/sysroot' :
+  wasmceptionDir + '/sysroot';
+
+exports.llvmDir = onServer ?
+  homeDir + '/clang' :
+  wasmceptionDir + '/dist';
+
+
+//  extension command for wasmGC and chisel and wasmdis
+
+exports.wasmGCCmd = homeDir + '/.cargo/bin/wasm-gc';
+
+exports.chiselCmd = homeDir + '/.cargo/bin/chisel';
+
+exports.wasmdisCmd = homeDir + '/binaryen/bin/wasm-dis';
+
+exports.tempDir = "/tmp";
+
